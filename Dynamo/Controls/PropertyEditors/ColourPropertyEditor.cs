b2701ea74e6f354d62cd90cf58df732e31ad5d98
Alex Davies <alex@frogtrade.com>
2022-01-19 07:47:37 +0000

    Added Colour Value Node
    Added reference to PixiEditor.ColorPicker and added a (temporary) ColourPropertyEditor and ColourPropertyNode

---
+++ Dynamo/Controls/PropertyEditors/ColourPropertyEditor.cs
@@ -0,0 +1,68 @@
+﻿using NodeGraph.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using Dynamo.Converters;
+using System.Diagnostics;
+
+namespace Dynamo.Controls.PropertyEditors
+{
+    public class ColourPropertyEditor : PropertyEditor
+    {
+        public string Value
+        {
+            get { return (string)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+        public static readonly DependencyProperty ValueProperty =
+            DependencyProperty.Register(
+                "Value",
+                typeof(System.Windows.Media.Color),
+                typeof(ColourPropertyEditor),
+                new PropertyMetadata(
+                    System.Windows.Media.Color.FromRgb(255, 255, 255),
+                    null,
+                    CoerceValue),
+                new ValidateValueCallback(IsValid));
+
+        public ColourPropertyEditor()
+        {
+            DataContext = this;
+        }
+
+        public static bool IsValid(object value)
+        {
+            return true;
+            /*
+            string stringValue = value as string;
+            if (stringValue == null || stringValue.Length == 0)
+                return true;
+            if (float.TryParse(stringValue, out float floatValue))
+                return true;
+            return false;
+            */
+        }
+
+        public static object CoerceValue(DependencyObject sender, object value)
+        {
+            return value;
+            /*
+            if (sender is IntPropertyEditor)
+            {
+                string stringValue = value as string;
+                if (float.TryParse(stringValue, out float floatValue))
+                {
+                    return ((int)floatValue).ToString();
+                }
+            }
+            return null;
+            */
+        }
+
+        public override void SetValueBinding(Port port)
+        {
+            SetBinding(ValueProperty, GetBinding(port));
+        }
+    }
+}
---
+++ Dynamo/Model/Nodes/ColourPropertyNode.cs
@@ -0,0 +1,46 @@
+﻿using NodeGraph.Model;
+using SixLabors.ImageSharp;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Text;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.PixelFormats;
+using Dynamo.Controls.PropertyEditors;
+using System.Xml;
+
+namespace Dynamo.Model
+{
+    [Node("Input/Colour", 103)]
+    public class ColourPropertyNode : ExecutableNode
+    {
+        [Port("Text", true, typeof(System.Windows.Media.Color), typeof(ColourPropertyEditor), false)]
+        public System.Windows.Media.Color Colour = System.Windows.Media.Colors.White;
+
+        [Port("Value", false, typeof(System.Windows.Media.Color), null)]
+        public System.Windows.Media.Color Value;
+
+        public ColourPropertyNode(Guid guid, Flowchart owner) : base(guid, owner)
+        {
+        }
+
+        public override void Execute()
+        {
+            Value = Colour;
+        }
+
+        public override void WriteXml(XmlWriter writer)
+        {
+            writer.WriteAttributeString("Colour", Colour.ToString());
+
+            base.WriteXml(writer);
+        }
+
+        public override void ReadXml(XmlReader reader)
+        {
+            //Colour = System.Windows.Media.Color.Fr(reader.GetAttribute("Colour"));
+
+            base.ReadXml(reader);
+        }
+    }
+}
